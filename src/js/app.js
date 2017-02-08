f9a790cb3324593768bdedcc46125fe7112e32df
mugukamil <mugukamil@gmail.com>
2017-02-08 13:54:28 +0300

    update

--- src/js/app.js
+++ src/js/app.js
@@ -26,16 +26,52 @@ $(document).ready(function() {
     });
 
     $('.main-slider__sync').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
+        slidesToShow: 3,
+        // slidesToScroll: 0,
         asNavFor: '.main-slider__img',
         centerMode: false,
         focusOnSelect: true,
         infinite: false,
         prevArrow: false,
         nextArrow: false,
+    });
 
-    })
+    $(document).ready(function() {
+        // Configure/customize these variables.
+        var showChar = 300;  // How many characters are shown by default
+        var ellipsestext = "...";
+        var moretext = "Читать далее>";
+        var lesstext = "Скрыть";
+
+
+        $('.more').each(function() {
+            var content = $(this).html();
+
+            if(content.length > showChar) {
+
+                var c = content.substr(0, showChar);
+                var h = content.substr(showChar, content.length - showChar);
+
+                var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+                $(this).html(html);
+            }
+
+        });
+
+        $(".morelink").click(function(){
+            if($(this).hasClass("less")) {
+                $(this).removeClass("less");
+                $(this).html(moretext);
+            } else {
+                $(this).addClass("less");
+                $(this).html(lesstext);
+            }
+            $(this).parent().prev().toggle();
+            $(this).prev().toggle();
+            return false;
+        });
+    });
 
     $('#iFrame1').iFrameResize({
         log: false
